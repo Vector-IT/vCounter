2c700770d31a815d6fd15c833e85ce58b652c6d8
José Romero <jose.romero@vector-it.com.ar>
2018-03-08 11:13:11 -0300

    Mejoras varias

--- src/vCounter.js
+++ src/vCounter.js
@@ -1,43 +1,43 @@
-(function ( $ ) {
- 
-    $.fn.vCounter = function() {
-        return this.each(function(i, elem) {
-            $(elem).prop("animando", false);
-            if (isScrolledIntoView(elem)) {
-                animarcount(elem);
-            }
+(function( $ ) {
 
-            $(document).scroll(function() {
-                if (isScrolledIntoView(elem)) {
-                    animarcount(elem);
-                }
-            });
+	$.fn.vCounter = function() {
+		return this.each(function(i, elem) {
+			$(elem).prop('animando', false);
+			if (isScrolledIntoView(elem)) {
+				animarcount(elem);
+			}
 
-            function animarcount(elem) {
-                if (!$(elem).prop("animando")) {
-                    $(elem).prop("animando", true);
-                    $(elem).prop('contador', 0).animate({
-                        contador: $(elem).text()
-                    }, {
-                        duration: 1000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
-                }
-            }
+			$(document).scroll(function() {
+				if (isScrolledIntoView(elem)) {
+					animarcount(elem);
+				}
+			});
 
-            function isScrolledIntoView(elem) {
-                var docViewTop = $(window).scrollTop();
-                var docViewBottom = docViewTop + $(window).height();
+			function animarcount(elem) {
+				if (!$(elem).prop('animando')) {
+					$(elem).prop('animando', true);
+					$(elem).prop('contador', 0).animate({
+						contador: $(elem).text()
+					}, {
+						duration: 1000,
+						easing: 'swing',
+						step: function(now) {
+							$(this).text(Math.ceil(now));
+						}
+					});
+				}
+			}
 
-                var elemTop = $(elem).offset().top;
-                var elemBottom = elemTop + $(elem).height();
+			function isScrolledIntoView(elem) {
+				var docViewTop = $(window).scrollTop();
+				var docViewBottom = docViewTop + $(window).height();
 
-                return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-            }
-        });
-    };
- 
-}( jQuery ));
+				var elemTop = $(elem).offset().top;
+				var elemBottom = elemTop + $(elem).height();
+
+				return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+			}
+		});
+	};
+
+})( jQuery );
